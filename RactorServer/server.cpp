f97bfe0809a93f06cce317a1f90db5228833188c
Choulee <zhxl8279@gmail.com>
2025-07-20 22:59:33 +0800

    02

--- RactorServer/server.cpp
+++ RactorServer/server.cpp
@@ -16,22 +16,13 @@
 
 extern int h_errno;
 
-int setnonblocking(int fd) {
-	int flags;
-
-	if ((flags=fcntl(fd, F_GETFL, 0)) == -1) {
-		flags = 0;
-	}
-	return fcntl(fd, F_SETFL, flags|O_NONBLOCK);
-}
-
 int main(int argc, char **argv) {
 	if (argc < 2) {
 		std::cerr << "Usage: ./server 8279" << std::endl;
 		exit(0);
 	}
 	int listenfd;
-    listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    listenfd = socket(AF_INET, SOCK_STREAM|SOCK_NONBLOCK, 0);
 	if (listenfd < 0) {
 		perror("socket");
 		exit(0);
@@ -42,8 +33,6 @@ int main(int argc, char **argv) {
 	setsockopt(listenfd, SOL_SOCKET, SO_REUSEPORT, &optval, sizeof(optval));
 	setsockopt(listenfd, SOL_SOCKET, SO_KEEPALIVE, &optval, sizeof(optval));
 	setsockopt(listenfd, TCP_NODELAY, SO_KEEPALIVE, &optval, sizeof(optval));
-
-    setnonblocking(listenfd);
 
 	struct sockaddr_in serverAddr;
 	serverAddr.sin_family = AF_INET;
@@ -92,26 +81,25 @@ int main(int argc, char **argv) {
 		} else {
 			for (int i = 0; i < retval; ++i) {
 				struct epoll_event &tmp = evs[i];
-				if (tmp.data.fd == listenfd) {
-					if ((clientfd = accept(tmp.data.fd, (struct sockaddr*)&clientAddr, &len)) < 0) {
-						perror("accept()");
-					} else {
-						setnonblocking(clientfd);
-						ev.data.fd = clientfd;
-						ev.events = EPOLLIN|EPOLLET;
-						if (epoll_ctl(epfd, EPOLL_CTL_ADD, clientfd, &ev) < 0) {
-							perror("epoll_ctl");
-							continue;
+				if (evs[i].events & EPOLLRDHUP) {
+					printf("client fd(%d) %s:%d disconnected\n", tmp.data.fd, inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
+					close(tmp.data.fd);
+				} else if (evs[i].events & (EPOLLIN | EPOLLPRI)) {
+					if (tmp.data.fd == listenfd) {
+						if ((clientfd = accept4(tmp.data.fd, (struct sockaddr*)&clientAddr, &len, SOCK_NONBLOCK)) < 0) {
+							perror("accept()");
+						} else {
+							ev.data.fd = clientfd;
+							ev.events = EPOLLIN|EPOLLET;
+							if (epoll_ctl(epfd, EPOLL_CTL_ADD, clientfd, &ev) < 0) {
+								perror("epoll_ctl");
+								continue;
+							}
+							++totalClientNum, ++activeClientNum;
+							printf("accept client[%llu/%llu] %s:%d ok\n", activeClientNum, totalClientNum,
+									inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
 						}
-						++totalClientNum, ++activeClientNum;
-                        printf("accept client[%llu/%llu] %s:%d ok\n", activeClientNum, totalClientNum,
-						       inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
-					}
-				} else {
-                    if (evs[i].events & EPOLLRDHUP) {
-						printf("client fd(%d) %s:%d disconnected\n", tmp.data.fd, inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port)); 
-						close(tmp.data.fd);
-					} else if (evs[i].events & (EPOLLIN | EPOLLPRI)) {
+					} else {
 						std::string sendBuf, recvBuf;
 						ssize_t byteN = 1024, recvN = 0;
 						while (true) {
@@ -124,26 +112,26 @@ int main(int argc, char **argv) {
 							} else if (recvN < 0 && (errno == EAGAIN || errno == EWOULDBLOCK)) {
 								break;
 							} else if (recvN < 0 && errno == EINTR) {
-							    continue;
+								continue;
 							} else if (recvN == 0) {
-							    printf("client fd(%d) %s:%d disconnected\n", tmp.data.fd, inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port)); 
-							    close(tmp.data.fd);
+								printf("client fd(%d) %s:%d disconnected\n", tmp.data.fd, inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
+								close(tmp.data.fd);
 								--activeClientNum;
-                                break;
+								break;
 							} else {
 								perror("recv");
-							    close(tmp.data.fd);
+								close(tmp.data.fd);
 								--activeClientNum;
-                                break;
+								break;
 							}
 						}
-					} else if (evs[i].events & EPOLLOUT) {
-
-					} else {
-					    printf("client fd(%d) %s:%d error\n", tmp.data.fd, inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port)); 
-					    close(tmp.data.fd);
-					    --activeClientNum;
 					}
+				} else if (evs[i].events & EPOLLOUT) {
+
+				} else {
+					printf("client fd(%d) %s:%d error\n", tmp.data.fd, inet_ntoa(clientAddr.sin_addr), ntohs(clientAddr.sin_port));
+					close(tmp.data.fd);
+					--activeClientNum;
 				}
 			}
 		}
